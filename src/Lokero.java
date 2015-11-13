d70b966c9bd6760bf3ff3980044a238436dc6eb9
Odixi <vz4ev4r@gmail.com>
2015-11-13 16:25:22 +0200

    Konstruktori

--- src/Lokero.java
+++ src/Lokero.java
@@ -1,12 +1,12 @@
 
 public class Lokero {
 
-	//private String nimi; //Turha?
-	
-	public Lokero(){
-		
-		//Kirjoitan turhan kommentin.	
-		
-	}
+	int[][] summat; //Summauspalvelijoiden summien lista ja niiden portit
+	int summaustenLkm;
+
+	public Lokero(int lkm){
+		this.summat = new int[lkm][2];
+		this.summaustenLkm = 0;
+	}//Konstruktori
 	
 }
