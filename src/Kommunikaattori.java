a13cd8101da641c4b8c9ed9bf4f549b3ecdb803a
Rikkokiri <pilvi.rajala@gmail.com>
2015-11-18 14:12:41 +0200

    Kommunikaattoria editoitua

--- src/Kommunikaattori.java
+++ src/Kommunikaattori.java
@@ -1,4 +1,45 @@
+import java.io.*;
+import java.net.*;
 
-public class Kommunikaattori {
+public class Kommunikaattori extends Thread{
 
+	private Socket socket;
+	private ObjectOutputStream out;
+	private ObjectInputStream in;
+	
+	private int portteja;
+	
+	public Kommunikaattori(Socket socket){
+		this.socket = socket;
+	}
+	
+	public void run(){
+		try{
+			
+			System.out.println("Käynnistetään Kommunikaattori-thread..."); //REMOVE
+
+			InputStream input = socket.getInputStream();
+			OutputStream output = socket.getOutputStream();
+			
+			ObjectOutputStream out = new ObjectOutputStream(output);
+			ObjectInputStream in = new ObjectInputStream(input);
+			
+			
+		}catch(IOException e){
+			//TODO
+		}
+		
+			
+			while(true){
+				try{
+					
+					portteja = in.readInt();
+					
+				}catch(IOException e){
+					//TODO
+				}
+			}
+		
+	}
+	
 }//Kommunikaattori
