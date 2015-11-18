909e39553b0ea7d8ad398e6bb105bb813da18edb
Rikkokiri <pilvi.rajala@gmail.com>
2015-11-18 02:53:07 +0200

    First attempt to make Kommunikaattori class and then tested version. Doesn't get response. Works otherwise. I guess?

---
+++ src/HOJTesti.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class HOJTesti {
+	
+	public static void main(String args[]) throws IOException{
+	
+		KommunikaattoriTesti kom = new KommunikaattoriTesti();
+		
+		kom.lahetaPortti();
+		
+		System.out.println("Done");
+		
+		
+	}
+}
