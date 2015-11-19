4cd31721e4ea641fe9a0fd87d8c16a367aa50921
Odixi <vz4ev4r@gmail.com>
2015-11-19 10:10:15 +0200

    X:n main metodi

--- src/X.java
+++ src/X.java
@@ -1,5 +1,18 @@
+import java.io.IOException;
+
 public class X{
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
+		
+		// Argumenttina ip osoite, ilman yhdistää local hostiin
+		
+		if (args.length == 0){
+			Yhteydenmuodostus uusiyhteys = new Yhteydenmuodostus();
+			uusiyhteys.lahetaPortti();
+		}
+		else{
+			Yhteydenmuodostus uusiyhteys = new Yhteydenmuodostus(args[0]);
+			uusiyhteys.lahetaPortti();
+		}
 		
 	}//main
 
